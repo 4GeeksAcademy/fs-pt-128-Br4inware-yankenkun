a2c52477541488139dafa946ef9b85c33c8635ef
Br4inware <bsabusiness@outlook.com>
2025-11-21 20:50:42 +0000

    Perdido

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
+
+//write your code here
+
+const choises = ["rock", "paper", "scissors", "lizard", "spock"]
+const rules = {
+  rock: ["scissors", "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"],
+}
+
+const jugar = (userChoise) => {
+
+  let computerChoise = choises[Math.floor(Math.random() * 5)]
+  let resultado
+  if (userChoise === computerChoise) {
+
+    resultado = "Esto es un empate!"
+
+  } else {
+
+    Object.keys(rules).forEach(choise => {
+
+      rules[choise].forEach(item => {
+        console.log(resultado)
+        if (rules[choise].includes(userChoise)) {
+          resultado = "Has Ganado"
+        } else{
+          resultado= "Has Perdido"
+        }
+      });
+    });
+  }
+
+  return resultado
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(jugar("rock"));
+
+
+//  document.getElementById("yankenkun").textContent = jugar ("spock");
+
+
+
+
